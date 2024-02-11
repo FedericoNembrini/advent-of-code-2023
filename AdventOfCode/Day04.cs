c1e1c851a7ec7e761d4357d60167cddc290b9bfa
FedericoNembrini <25205086+FedericoNembrini@users.noreply.github.com>
2024-02-11 22:14:54 +0100

    [main] Improve day 04 part 02 solution

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -22,8 +22,8 @@ public class Day04 : BaseDay
             _input[i] = _input[i].Substring(_input[i].IndexOf(':') + 1).Trim();
             cards.Add(new Card()
             {
-                LuckyNumbers = _input[i].Split('|')[0].Split(' ').Where(i => !string.IsNullOrEmpty(i)).Select(i => Convert.ToInt32(i)).ToList(),
-                CardNumbers = _input[i].Split('|')[1].Split(' ').Where(i => !string.IsNullOrEmpty(i)).Select(i => Convert.ToInt32(i)).ToList(),
+                LuckyNumbers = _input[i].Split('|')[0].Split(' ').Where(i => !string.IsNullOrEmpty(i)).Select(i => Convert.ToInt32(i)).ToHashSet(),
+                CardNumbers = _input[i].Split('|')[1].Split(' ').Where(i => !string.IsNullOrEmpty(i)).Select(i => Convert.ToInt32(i)).ToHashSet(),
             });
         }
 
@@ -36,24 +36,30 @@ public class Day04 : BaseDay
 
     private int SolvePart02()
     {
-        List<Card> cards = [];
+        Dictionary<int, Card> originalCards = [];
 
         for (int i = 0; i < _input.Length; i++)
         {
             _input[i] = _input[i].Substring(_input[i].IndexOf(':') + 1).Trim();
-            cards.Add(new Card()
+
+            Card card = new()
             {
                 Id = i + 1,
-                LuckyNumbers = _input[i].Split('|')[0].Split(' ').Where(i => !string.IsNullOrEmpty(i)).Select(i => Convert.ToInt32(i)).ToList(),
-                CardNumbers = _input[i].Split('|')[1].Split(' ').Where(i => !string.IsNullOrEmpty(i)).Select(i => Convert.ToInt32(i)).ToList(),
-            });
+                LuckyNumbers = _input[i].Split('|')[0].Split(' ').Where(i => !string.IsNullOrEmpty(i)).Select(i => Convert.ToInt32(i)).ToHashSet(),
+                CardNumbers = _input[i].Split('|')[1].Split(' ').Where(i => !string.IsNullOrEmpty(i)).Select(i => Convert.ToInt32(i)).ToHashSet(),
+            };
+
+            _ = card.GetWinNumberCount();
+            originalCards.Add(card.Id, card);
         }
+
+        List<Card> cards = new List<Card>(originalCards.Values);
 
         for (int x = 0; x < cards.Count; x++)
         {
             for (int y = 0; y < cards[x].GetWinNumberCount(); y++)
             {
-                cards.Add(cards.First(c => c.Id == (cards[x].Id + y + 1)));
+                cards.Add(originalCards[cards[x].Id + y + 1]);
             }
         }
 
@@ -65,9 +71,11 @@ public class Card
 {
     public int Id { get; set; }
 
-    public List<int> LuckyNumbers { get; set; } = [];
+    public HashSet<int> LuckyNumbers { get; set; } = [];
 
-    public List<int> CardNumbers { get; set; } = [];
+    public HashSet<int> CardNumbers { get; set; } = [];
+
+    private int? _winNumberCount = null;
 
     public int GetCardPoints()
     {
@@ -86,7 +94,12 @@ public class Card
 
     public int GetWinNumberCount()
     {
+        if (_winNumberCount.HasValue)
+            return _winNumberCount.Value;
+
+        _winNumberCount = LuckyNumbers.Intersect(CardNumbers).Count();
+
         return
-            LuckyNumbers.Intersect(CardNumbers).Count();
+            _winNumberCount.Value;
     }
 }
